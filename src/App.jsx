210335f881cd8fec0f37e1b549c559248d504d1d
unknown <rohit_chandel>
2024-06-21 12:17:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Comm Project</h1>
+      <button>Normal Button</button>
+      <Button variant="primary">Bootstrap Button</Button>
+    </div>
+  );
+}
+
+
+export default App;
